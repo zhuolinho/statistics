1461dfb850350060f0033162d626340e1882a487
Jolin.H <zhuolinho@hotmail.com>
2017-06-01 14:44:37 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+// var DB_CONN_STR = 'mongodb://tcc:tcc@image.limadcw.com:27017/tcc';
+var DB_CONN_STR = 'mongodb://tcc:tcc@192.168.10.200:27017/tcc';
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    // MongoClient.connect(DB_CONN_STR, function (err, db) {
+    //     var arr = {};
+    //     db.collection('conch_ChargeAppealSum').find({periodType: 'MONTH'}).forEach(function (doc) {
+    //         arr[doc._id] = doc;
+    //
+    //     });
+    //     db.close();
+    // });
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+        host     : '54.222.179.73',
+        user     : 'image',
+        password : 'image@thinkLight',
+        database : 'image'
+    });
+    connection.connect();
+    connection.query('SELECT * FROM image.tb_park_plate LIMIT 0, 10', function (error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+    });
+    connection.end();
+});
+
+module.exports = router;
