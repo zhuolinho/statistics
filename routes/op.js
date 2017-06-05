1f070fef79ee92d59d5e0302ed3759917075c682
Jolin.H <zhuolinho@hotmail.com>
2017-06-05 14:52:47 +0800

    trade

--- routes/op.js
+++ routes/op.js
@@ -21,15 +21,26 @@ router.get('/', function (req, res, next) {
             // database : 'image'
         });
         connection.connect();
-        connection.query("SELECT b.id, b.name, a.count, a.sum FROM (SELECT park_id, COUNT(*) AS COUNT, SUM(actual_fee) AS SUM FROM tb_park_charge_order WHERE crt_time BETWEEN '" + req.query.startDate + "' AND '" + req.query.endDate + "' AND order_category = 'OP' AND ispay = 'Y' AND STATUS = 'R' GROUP BY park_id) AS a, tb_park_park AS b WHERE a.park_id = b.id", function (error, results, fields) {
+        connection.query("SELECT a.park_id, b.name, a.count, a.sum FROM (SELECT park_id, COUNT(*) AS COUNT, SUM(actual_fee) AS SUM FROM tb_park_charge_order WHERE crt_time BETWEEN '" + req.query.startDate + "' AND '" + req.query.endDate + "' AND order_category = 'OP' AND ispay = 'Y' AND STATUS = 'R' GROUP BY park_id) AS a, tb_park_park AS b WHERE a.park_id = b.id", function (error, results, fields) {
             if (error) throw error;
             var orders = {};
             results.forEach(function (order) {
-                orders[order.client_id] = order;
+                orders[order.id] = order;
             });
-            res.send(orders);
-            connection.end();
-            querying = false;
+            connection.query("SELECT a.park_id, b.name, a.c, a.s FROM (SELECT park_id, COUNT(*) AS c, SUM(price) AS s FROM tb_park_cost_trade WHERE create_time BETWEEN '" + req.query.startDate + "' AND '" + req.query.endDate + "' AND TYPE = 'CONSUME' GROUP BY park_id) AS a, tb_park_park AS b WHERE a.park_id = b.id", function (error, results, fields) {
+                if (error) throw error;
+                results.forEach(function (trade) {
+                    if (orders[trade.park_id]) {
+                        orders[trade.park_id].c = trade.c;
+                        orders[trade.park_id].s = trade.s;
+                    } else {
+                        orders[trade.park_id] = trade;
+                    }
+                });
+                res.send(orders);
+                connection.end();
+                querying = false;
+            });
         });
     }
 });
