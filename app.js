f254f95896c3305e4677aed75a420e5815711219
Jolin.H <zhuolinho@hotmail.com>
2017-06-05 14:39:42 +0800

    app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var qr = require('./routes/qr');
+var op = require('./routes/op');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/qr', qr);
+app.use('/op', op);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
