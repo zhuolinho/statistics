0a80b2c973252476fc16addd75e90f995574a4a2
Jolin.H <zhuolinho@hotmail.com>
2017-07-04 09:48:38 +0800

    startTime

--- routes/pay.js
+++ routes/pay.js
@@ -30,7 +30,11 @@ router.get('/', function (req, res, next) {
         co(function*() {
             var db = yield MongoClient.connect(DB_CONN_STR);
             var endTime = new Date(req.query.endTime);
-            for (var startTime = new Date(req.query.startTime); startTime < endTime;) {
+            var startTime = new Date(req.query.startTime);
+            startTime.setHours(0);
+            startTime.setMinutes(0);
+            startTime.setSeconds(0);
+            for (; startTime < endTime;) {
                 var condition = "";
                 if (req.query.parkIds) {
                     var arr = req.query.parkIds.split(",");
