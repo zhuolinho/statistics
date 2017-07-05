2a1455b274c7f858847af4adfea5f1e58ccf54b4
Jolin.H <zhuolinho@hotmail.com>
2017-07-05 15:23:16 +0800

    114.55.3.90

--- routes/duration.js
+++ routes/duration.js
@@ -14,7 +14,7 @@ var ObjectID = require('mongodb').ObjectID;
 
 router.get('/', function (req, res, next) {
     var remoteAddress = req.connection.remoteAddress.split(":")[3];
-    if (querying || remoteAddress != "211.161.198.70" && remoteAddress) res.send(remoteAddress);
+    if (querying || remoteAddress != "211.161.198.70" && remoteAddress != "114.55.3.90" && remoteAddress) res.send(remoteAddress);
     else {
         querying = true;
         var connection = mysql.createConnection({
