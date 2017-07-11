1a0fe01250c4824009d53526542a0d7700fc5897
Jolin.H <zhuolinho@hotmail.com>
2017-07-11 10:07:42 +0800

    remoteAddress

--- routes/ahole.js
+++ routes/ahole.js
@@ -8,8 +8,8 @@ var querying = false;
 var co = require('co');
 
 router.get('/', function (req, res, next) {
-    if (querying || req.connection.remoteAddress.split(":")[3] == "211.161.198.70") {
-        res.send("querying...");
+    if (querying || remoteAddress != "211.161.198.70" && remoteAddress != "114.55.3.90" && remoteAddress) {
+        res.send(remoteAddress);
     }
     else {
         querying = true;
